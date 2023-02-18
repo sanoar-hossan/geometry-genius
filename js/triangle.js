304fd0d7d5fc51ba53512a596eb4fdf7e30b45ef
chayan <admin@ditskill.com>
2023-02-18 19:40:18 +0600

    triangele part close

--- js/triangle.js
+++ js/triangle.js
@@ -1,4 +1,17 @@
 document.getElementById('triangle-btn').addEventListener('click',function () {
-    console.log('object');
+    
+
+    const triangleBase=getInputValue('tbi');
+    const triangleHeight=getInputValue('thi');
+//validation for input
+if (isNaN(triangleBase)|| triangleBase<=0 || isNaN(triangleHeight) || triangleHeight<=0) {
+    alert("Please enter valid values for Base and Height");
+    return;
+  }
+
+
+
+    const triangleArea=0.5*triangleBase*triangleHeight;
+    setInnerText('tm',triangleArea)
     
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,20 @@
 function getInputValue(inputId) {
-    const inputElement=document.getElementById('inputId')
+    const inputElement=document.getElementById(inputId);
+    const inputElementString=inputElement.value;
+    const inputValue=parseFloat(inputElementString);
+    inputElement.value='';
+    return inputValue;
+}
+function getInnerText(inputId) {
+    const innerElement=document.getElementById(inputId);
+    const innerElementString=innerElement.innerText;
+    const innerValue=parseFloat(innerElementString);
+    innerElement.innerText='';
+    return innerValue;
+}
+function setInnerText(inputId,value) {
+    const getId=document.getElementById(inputId);
+    getId.innerText=value;
     
+   
 }
